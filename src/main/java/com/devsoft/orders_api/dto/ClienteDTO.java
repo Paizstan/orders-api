863d4c5a61096a3b47a859160624c9a9644f2c2a
Stanley <ps24001@itcha.edu.sv>
2025-08-11 08:33:48 -0600

    archivos DTO

---
+++ src/main/java/com/devsoft/orders_api/dto/ClienteDTO.java
@@ -0,0 +1,10 @@
+package com.devsoft.orders_api.dto;
+
+public class ClienteDTO {
+    private Long id;
+    private String nombre;
+    private String direccion;
+    private String telefono;
+    private String email;
+    private String tipoCliente;
+}
---
+++ src/main/java/com/devsoft/orders_api/dto/RoleDTO.java
@@ -0,0 +1,7 @@
+package com.devsoft.orders_api.dto;
+
+public class RoleDTO {
+    private Long id;
+    private String nombre;
+
+}
