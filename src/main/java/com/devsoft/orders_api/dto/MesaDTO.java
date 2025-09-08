6bff7662d3044c1a2c05918bb4d24faebe574149
Stanley <ps24001@itcha.edu.sv>
2025-09-08 10:49:03 -0600

    reportes

--- src/main/java/com/devsoft/orders_api/dto/MesaDTO.java
+++ src/main/java/com/devsoft/orders_api/dto/MesaDTO.java
@@ -12,7 +12,7 @@ import lombok.NoArgsConstructor;
 @Builder
 public class MesaDTO {
     private Long id;
-    private  String numero;
+    private  Integer numero;
     private String ubicacion;
 
 }
